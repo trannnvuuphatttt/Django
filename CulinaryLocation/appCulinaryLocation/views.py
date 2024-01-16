c7465a8b490f229d1f6edb0c4718cf31a5f6d0d7
trannnvuuphat <tranvuphato@gmail.com>
2024-01-16 08:48:06 +0700

    new

--- CulinaryLocation/appCulinaryLocation/views.py
+++ CulinaryLocation/appCulinaryLocation/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 # Create your views here.
 def hello(request):
